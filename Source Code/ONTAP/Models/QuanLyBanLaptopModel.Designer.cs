91b39a93c803740a6405159d9de0881390668be8
Ambition1999 <Ambition1999@users.noreply.github.com>
2020-09-11 01:00:59 +0700

    ABC
    Source code trang web bán laptop và thiết bị điện tử

---
+++ Source Code/ONTAP/Models/QuanLyBanLaptopModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\USER\Documents\Zalo Received Files\Đồ án Kiểm Thử Chất Lượng Phần Mềm Final\DoAnKiemThuChatLuongPhanMem_icon\web\ONTAP\ONTAP\ONTAP\Models\QuanLyBanLaptopModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
